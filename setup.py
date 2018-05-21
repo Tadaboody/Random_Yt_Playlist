705c5d5140caf3395787cd8f0eaa285d44e23a46
Tadaboody <tomerpet@gmail.com>
2018-05-22 00:04:21 +0300

    initial

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name='random-yt-playlist',
+    version='0.1',
+    py_modules=['script'],
+    install_requires=[
+        'click','requests','bs4'
+    ],
+    entry_points='''
+        [console_scripts]
+        random_yt_playlist=script:cli
+    ''',
+)

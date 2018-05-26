2cc23ef9f4d43907746bd9fdd1c48e18d8aaf163
Tadaboody <tomerpet@gmail.com>
2018-05-26 17:28:29 +0300

    Cache playlist list for reloading

--- browser_extention/background.js
+++ browser_extention/background.js
@@ -1,8 +1,9 @@
+var playlist_list;
 chrome.browserAction.onClicked.addListener(function (tab) {
     console.log('icon clicked');
     get_yt_html(
         (html) => {
-            const playlist_list = get_playlists(html);
+            playlist_list = playlist_list || get_playlists(html);
             const chosen_playlist = unescape(JSON.parse('"' + choose(playlist_list))); //Unescape unicode characters like \\u200;
             open_url(yt_endpoint(chosen_playlist));
         }
@@ -10,10 +11,10 @@ chrome.browserAction.onClicked.addListener(function (tab) {
     // open_url(yt_endpoint(),{new_window : true});
 });
 
-function choose(array)
-{
+function choose(array) {
     return array[Math.floor(Math.random() * array.length)];
 }
+
 function yt_endpoint(...args) {
     const base_url = "https://www.youtube.com";
     return args.reduce((acc, current) => acc + '/' + current, base_url);
@@ -32,16 +33,15 @@ function get_yt_html(cb) {
     return xhr.responseText;
 }
 
-function get_playlists(html)
-{
+function get_playlists(html) {
     const playlist_regex = /(watch\?v=[^\"]+list=[^\"]+)\"/g;
     const playlist_list = html.match(playlist_regex);
     console.log(playlist_list);
     return playlist_list;
 }
 
-function open_url(url, parameters={active : true, pinned : false, new_window : false}) {
-    
+function open_url(url, parameters = { active: true, pinned: false, new_window: false }) {
+
     if (parameters.new_window) {
         chrome.windows.create({ url: url });
     }

ef8339c1c3813fd329f18a5d013838357b899a8a
Tadaboody <tomerpet@gmail.com>
2018-06-09 14:10:26 +0300

    Read for pypi release

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup
 
+with open("README.md") as fp:
+    long_description = fp.read()
 
 setup(
     name='random_yt_playlist',
     version='0.1',
+    author="Tomer Keren",
+    author_email="tomer.keren.dev@gmail.com",
+    description="A utility to open a random youtube playlist",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Tadaboody/Random_Yt_Playlist",
     packages=['random_yt_playlist'],
     install_requires=[
         'click','requests','click_default_group','browser_cookie3'
@@ -12,4 +20,9 @@ setup(
         [console_scripts]
         random_yt_playlist=random_yt_playlist.script:cli
     ''',
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
 )
